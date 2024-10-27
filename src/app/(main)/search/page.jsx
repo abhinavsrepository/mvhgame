663507dd2897d36af5a0b7204fc4f58bbf708f64
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-10-27 18:01:44 +0530

    search functionality added

--- src/app/(main)/search/page.jsx
+++ src/app/(main)/search/page.jsx
@@ -1,14 +1,45 @@
 import { PrismaClient } from "@prisma/client";
+import Image from "next/image";
 const prisma = new PrismaClient();
-async function getDate(q) {
+async function getData(q) {
   const games = await prisma.game.findMany({
-    where: { title: { contains: q } },
+    where: {
+      title: {
+        contains: q,
+      },
+    },
+    take: 100,
   });
+  return games;
 }
 export default async function Page(req) {
+  const searchQuery = req.searchParams.q;
+  
+  let games;
+  if (searchQuery) {
+      games =await  getData(searchQuery);
+  }
+  else {
+    games=[];
+  }
+  
   return (
     <div>
-      <h1>search results</h1>
+      <h1 className="font-display text-2xl md:text-3xl mb-4">
+        {searchQuery?`Search result for ${searchQuery}`:
+        "No search result provided"}
+        
+      </h1>
+      <div className="text-accent mb-4">{`${games?.length} results`}</div>
+      <ul>{games. map((game)=>(
+        <li key={game.id}>
+          <a href={`/game/${game.slug}`} className="flex ga-4 bg-main hover:bg-accent-secondary p-4 rounded-lg">
+          <Image src={`/game/${game.image}`} alt ={game.title}
+          className="w-2/6 lg:w-1/6"/>
+          </a>
+        </li>
+
+      ))}</ul>
     </div>
   );
 }
