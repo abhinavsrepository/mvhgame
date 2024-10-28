a3f0c657a43aa31ce578b24c916802d840d7b5f2
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-10-28 17:59:55 +0530

    initial commit

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,12 @@
+import CategorySlider from "@/components/Sliders/CategorySlider";
 import HeroSlider from "@/components/Sliders/HeroSlider";
 
 export default function Home() {
   return (
    <>
    <HeroSlider/>
+ 
+
    </>
   );
 }
